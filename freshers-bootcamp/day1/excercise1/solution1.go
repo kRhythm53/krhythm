0c3210db43c15c5e6d443229030e2575b04c103e
Rhythm <35167328+kRhythm@users.noreply.github.com>
2021-07-21 17:27:26 +0530

    added solution3

--- freshers-bootcamp/day1/excercise1/solution1.go
+++ freshers-bootcamp/day1/excercise1/solution1.go
@@ -1,1 +1,54 @@
-package excercise1
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Matrix struct{
+	row int
+	col int
+	matrix [][]int
+}
+func (m Matrix)getRows() int{
+	return m.row
+}
+func (m Matrix)getCols() int{
+	return m.col
+}
+func(m *Matrix)setPosition(i int,j int,val int) {
+	m.matrix[i][j] = val
+}
+func(m Matrix)addMatrix(m2 Matrix) Matrix{
+
+	for i:=0;i<m.row;i++{
+		for j:=0;j<m.col;j++{
+			m.matrix[i][j] = m.matrix[i][j]+m2.matrix[i][j]
+		}
+	}
+	return m
+}
+func(m Matrix) printToJson(){
+
+	b,err := json.Marshal(m.matrix);
+	if err==nil {fmt.Println(string(b))}
+}
+func newMatrix() Matrix{
+	row1 := []int{1, 2, 3}
+	row2 := []int{4, 5, 6}
+
+	m1 := Matrix{2,3,nil}
+	m1.matrix = append(m1.matrix,row1)
+	m1.matrix = append(m1.matrix,row2)
+
+	return m1
+}
+func main(){
+	var m1 = newMatrix()
+	var m2 = newMatrix()
+	m1.setPosition(0,1,5)
+	fmt.Println(m1.getRows())
+	fmt.Println(m2.getCols())
+	fmt.Println(m1.addMatrix(m2))
+	m1.printToJson()
+}
--- freshers-bootcamp/day1/excercise3/solution3.go
+++ freshers-bootcamp/day1/excercise3/solution3.go
@@ -1,1 +1,33 @@
-package excercise3
+
+package main
+
+import "fmt"
+
+type Salary interface{
+	fulltime() int
+	contractor() int
+	freelancer() int
+}
+
+type employees struct{
+	fullTime int
+	cont int
+	freelance int
+}
+func(x employees) fulltime() int{
+	return x.fullTime*500
+}
+func(y employees) contractor() int{
+	return y.cont*100
+}
+func(z employees) freelancer() int{
+	return z.freelance*10
+}
+func main(){
+	var employee Salary
+	employee = employees{fullTime: 28,cont: 28,freelance: 200}
+
+	fmt.Println("fulltime employees  salary :",employee.fulltime())
+	fmt.Println("contractors salary: ",employee.contractor())
+	fmt.Println("freelancers salary: ",employee.freelancer())
+}
